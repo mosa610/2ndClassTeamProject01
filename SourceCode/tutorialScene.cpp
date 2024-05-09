b6a5f046dd13946fa8c49d110cbd12c6073ebb96
隼人 津守 <2230121@ecc.ac.jp>
2024-05-09 18:27:12 +0900

    a

--- SourceCode/tutorialScene.cpp
+++ SourceCode/tutorialScene.cpp
@@ -10,28 +10,23 @@ void TutorialScene::init()
 {
     timer = 0;
     stageNumber = 0;
-    GameScene::instance()->init();
 }
 
 void TutorialScene::deinit()
 {
-    GameScene::instance()->deinit();
 }
 
 void TutorialScene::update()
 {
-    GameScene::instance()->update();
 }
 
 void TutorialScene::draw()
 {
     GameLib::clear(1, 0, 1);
-    GameScene::instance()->draw();
     reset();
 }
 
 void TutorialScene::reset()
 {
-    GameScene::instance()->reset();
 }
     
