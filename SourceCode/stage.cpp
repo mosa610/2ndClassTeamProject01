e129a9aa0b0de4a9fc518088acfa6ac77de43172
西田翔星 <2230048@MSI>
2024-05-09 20:03:07 +0900

    stage1

--- SourceCode/stage.cpp
+++ SourceCode/stage.cpp
@@ -4,6 +4,13 @@ int stageNumber;
 
 Stage_script stage[4][7] = {
     {
+        {0,300,31,0.5f,200,true,false,1},
+        {20,700,31,0.5f,200,true,false,1},
+        {30,1100,30,2,200,true,false,0},
+        {-1,0,0}
+    },
+
+    {
         {0,200,30,2,100,true,false,0},
         {0,400,31,0.5f,100,true,false,1},
         {15,600,32,1,1500,false,true,1},
@@ -25,10 +32,13 @@ Stage_script stage[4][7] = {
         {10,400,31,0.5f,50,true,false,1},
         {20,1400,30,2,50,true,false,0},
         {-1,0,0}
-    }
+    },
 };
 
-Stage_script stageWeather[3][1] = {
+Stage_script stageWeather[4][1] = {
+    {
+        {30,0,0,0,0,0,0,0,true}
+    },
     {
         {-1,0,0}
     },
@@ -40,14 +50,17 @@ Stage_script stageWeather[3][1] = {
     },
 };
 
-Stage_script stageTime[3][1] = {
+Stage_script stageTime[4][1] = {
     {
-        {180,0,0}
+        {60,0,0},
     },
     {
         {180,0,0},
     },
     {
-        {180,0,0}
+        {180,0,0},
+    },
+    {
+        {180,0,0},
     },
 };
